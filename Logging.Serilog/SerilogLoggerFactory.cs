7a34cfdd294d21be2c02020c435215173ccd9b4b
Valtonen Johannes <johannes.valtonen@cgi.com>
2018-10-04 17:00:17 +0300

    Initial implementation of the Serilog logger.

---
+++ Logging.Serilog/SerilogLoggerFactory.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+
+namespace Affecto.Logging.Serilog
+{
+    public class SerilogLoggerFactory : LoggerFactory
+    {
+        private static global::Serilog.ILogger loggerSingleton;
+        private readonly object createLock = new object();
+
+        protected override ILogWriter GetLogWriter(object source)
+        {
+            if (loggerSingleton == null)
+            {
+                lock (createLock)
+                {
+                    if (loggerSingleton == null)
+                    {
+                        loggerSingleton = CreateLoggerConfiguration().CreateLogger();
+                    }
+                }
+            }
+
+            return new LogWriter(loggerSingleton);
+        }
+
+        protected virtual LoggerConfiguration CreateLoggerConfiguration()
+        {
+            return new LoggerConfiguration()
+                .Enrich.WithCaller();
+        }
+    }
+}
