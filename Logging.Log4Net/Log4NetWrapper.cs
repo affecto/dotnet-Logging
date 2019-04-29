ccca9d147dc61efda28e3ad49125adb048a80400
Martynas Ramonas <maras300@yahoo.com>
2019-04-29 14:40:19 +0300

    Hotfix for ASP.NET solutions (#9)
    Bring back support for ASP.NET solutions and added loading log4net configuration from web.config

--- Logging.Log4Net/Log4NetWrapper.cs
+++ Logging.Log4Net/Log4NetWrapper.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using log4net;
 using log4net.Config;
@@ -10,13 +14,39 @@ namespace Affecto.Logging.Log4Net
     {
         public virtual void Configure()
         {
-            log4net.Repository.ILoggerRepository logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            log4net.Repository.ILoggerRepository logRepository = LogManager.GetRepository(GetInitialAssembly());
+
+            var configFileInfo = new FileInfo("log4net.config");
+
+            if (configFileInfo.Exists)
+            {
+                XmlConfigurator.Configure(logRepository, configFileInfo);
+            }
+            else
+            {
+                XmlConfigurator.Configure(logRepository);
+            }
         }
 
         public virtual ILog GetLog(Type type)
         {
             return LogManager.GetLogger(type);
+        }
+
+        private Assembly GetInitialAssembly()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+
+            if (assembly == null)
+            {
+                Assembly currentAssembly = Assembly.GetExecutingAssembly();
+                IEnumerable<Assembly> callerAssemblies = new StackTrace().GetFrames()
+                    .Select(x => x.GetMethod().ReflectedType.Assembly).Distinct()
+                    .Where(x => x.GetReferencedAssemblies().Any(y => y.FullName == currentAssembly.FullName));
+                assembly = callerAssemblies.Last();
+            }
+
+            return assembly;
         }
     }
 }
